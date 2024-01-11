f0cff239041bd8b61532f8764d7f0719b031f092
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-11 12:34:47 +0900

    create table

--- spl/create.sql
+++ spl/create.sql
@@ -1,11 +1,46 @@
-CREATE TABLE Books(
-    book_id INT auto_increment NOT NULL,
-    google_books_apis VARCHAR(50) NOT NULL,
-    PRIMARY KEY(book_id)
-);
-
 CREATE TABLE Category(
     category_id INT auto_increment NOT NULL,
     category_name VARCHAR(50) NOT NULL,
     PRIMARY KEY(book_id)
+);
+
+CREATE TABLE Books(
+    book_id  VARCHAR(50) NOT NULL,
+    category_id INT NOT NULL,
+    PRIMARY KEY(book_id),
+    FOREIGN KEY(category_id) REFERENCES Category(category_id)
+);
+
+CREATE TABLE Administrators(
+    administrators_id  INT auto_increment NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    administrators_name  VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    registration_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    update_date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    last_access_date  DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY(administrators_id)
+);
+
+CREATE TABLE Users(
+    user_id  INT auto_increment NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    user_name  VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    registration_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    update_date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    last_access_date  DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY(user_id)
+);
+
+CREATE TABLE RegisteredBooks(
+    user_id  INT NOT NULL,
+    book_id VARCHAR(50) NOT NULL,
+    favorite INT DEFAULT 0 NOT NULL,
+    Possession INT DEFAULT 0 NOT NULL,
+    purchase_date DATETIME,
+    Purchase_amount INT,
+    PRIMARY KEY(users_id,book_id),
+    FOREIGN KEY(user_id) REFERENCES Users(user_id),
+    FOREIGN KEY(book_id) REFERENCES Books(book_id)
 );
