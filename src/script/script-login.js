3c44125cfb946ba76f3cb93eccb0970a6ec6293b
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-12 13:08:42 +0900

    a

--- src/script/script-login.js
+++ src/script/script-login.js
@@ -3,7 +3,7 @@ new Vue({
     data(){
         return{
             password1,
-            password2,
+            password2
         };
     },
     methods: {
@@ -11,8 +11,8 @@ new Vue({
     computed: {
         pasCheck(){
             const password1 = this.password1;
-            // const password2 = this.password1;
-            const isErr =  password1 < 8  || isNaN(Number(id));
+            const password2 = this.password1;
+            const isErr =  password1 == password2  ;
             return isErr;
         },
         s: function(){
