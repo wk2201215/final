b6a1162a94d61402c4738920f695a25cd61282cd
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-12 13:37:47 +0900

    a

--- src/login-input.php
+++ src/login-input.php
@@ -23,6 +23,6 @@
 ?>
     </form>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.min.js"></script>
 <script src="./script/script-login.js"></script>
 <?php require 'default/footer.php'; ?>
