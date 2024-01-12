aa5cefa41bcbfd63c9048dad0ec015a21bcae346
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-12 12:11:48 +0900

    login u

--- src/login-input.php
+++ src/login-input.php
@@ -4,11 +4,15 @@
     unset($_SESSION['customer']);
     echo 'ユーザーID';
     echo '<input type="text" name="login_id">';
+    echo '<br>';
     echo 'パスワード';
     echo '<input type="password" name="password1">';
+    echo '<br>';
     echo 'パスワード確認';
     echo '<input type="password" name="password2">';
+    echo '<br>';
     echo '<input type="submit" class="button" value="ログイン">';
+    echo '<br>';
     echo 'アカウント新規作成は<a href="customer-input.php">こちら</a>';
 ?>
     </form>
