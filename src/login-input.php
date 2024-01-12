1802d9aba0a4a0f3bece19b780b5579c84ca6eed
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-12 12:51:35 +0900

    henkou

--- src/login-input.php
+++ src/login-input.php
@@ -6,15 +6,24 @@
     echo '<input type="text" name="login_id">';
     echo '<br>';
     echo 'パスワード';
-    echo '<input type="password" name="password1">';
+    echo '<input type="password" name="password1" v-model="password1">';
     echo '<br>';
+    echo '<div class="help-block">
+           <div class="progress progress-striped active">
+            <div class="progress-bar progress-bar-info" :style="{width: `${s}%`}">
+            </div>
+          </div>
+         </div>';
     echo 'パスワード確認';
-    echo '<input type="password" name="password2">';
+    echo '<input type="password" name="password2" v-model="password2">';
     echo '<br>';
+    echo '<p  v-if="pasCheck">パスワードが違います</p>';
     echo '<input type="submit" class="button" value="ログイン">';
     echo '<br>';
     echo 'アカウント新規作成は<a href="customer-input.php">こちら</a>';
 ?>
     </form>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="./script/script-login.js"></script>
 <?php require 'default/footer.php'; ?>
---
+++ src/script/script-login.js
@@ -0,0 +1,28 @@
+new Vue({
+    el: '#app',
+    data(){
+        return{
+            password1,
+            password2,
+        };
+    },
+    methods: {
+    },
+    computed: {
+        pasCheck(){
+            const password1 = this.password1;
+            // const password2 = this.password1;
+            const isErr =  password1 < 8  || isNaN(Number(id));
+            return isErr;
+        },
+        s: function(){
+            let s = 0
+            s += /.{8,}/.test(this.password1) ? 20 : 0
+            s += /\d/.test(this.password1)    ? 20 : 0
+            s += /[a-z]/.test(this.password1) ? 20 : 0
+            s += /[A-Z]/.test(this.password1) ? 20 : 0        
+            s += /[#!&]/.test(this.password1) ? 20 : 0
+            return s
+        }
+    }
+});
