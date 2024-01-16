4d19518fa6061662dc6738ec17e7562427867786
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-16 11:55:10 +0900

    a

--- src/customer-input.php
+++ src/customer-input.php
@@ -1,5 +1,5 @@
 <?php session_start(); ?>
-<?php require 'db-connect.php'; ?>
+<?php require 'default/db-connect.php'; ?>
 <?php require 'default/header.php'; ?>
 <?php
     $id=$password=$name=$email='';
