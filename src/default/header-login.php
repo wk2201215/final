02d64a5cc39800140aa4e2a4f2473404f0e82d8b
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-16 12:25:55 +0900

    a

--- src/default/header-login.php
+++ src/default/header-login.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>alcoholdrink</title>
+<title>books</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- css -->
 <link rel="stylesheet" href="css/login.css"/>
