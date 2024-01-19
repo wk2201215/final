e4a47f1580ce6aab31da5da3e12ef7ad09201aa4
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-20 04:31:06 +0900

    a

--- src/default/footer-top.php
+++ src/default/footer-top.php
@@ -1,4 +1,4 @@
 </body>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="top-t.js"></script>
 </html>
