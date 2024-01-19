9158af8c37d12aa5a27e2ff26d61b5ff2cad82f8
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-19 21:36:52 +0900

    a

--- src/default/db-connect.php
+++ src/default/db-connect.php
@@ -1,7 +1,7 @@
 <?php
     const SERVER = 'mysql220.phy.lolipop.lan';
-    const DBNAME = 'LAA1517449-final';
-    const USER = 'LAA1517449';
+    const DBNAME = 'LAA1517442-final';
+    const USER = 'LAA1517442';
     const PASS = 'final2024';
 
     $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
