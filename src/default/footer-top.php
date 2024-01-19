b1437e15b124ebe05db9199a4831f6c8fc9fe84d
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-20 04:21:39 +0900

    a

--- src/default/footer-top.php
+++ src/default/footer-top.php
@@ -1,4 +1,4 @@
 </body>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="top-t.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </html>
