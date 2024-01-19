2ef3e0b9c02f622c08973c6f56f8d25335d8c873
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-20 05:26:28 +0900

    あ

--- src/search.php
+++ src/search.php
@@ -1,7 +1,7 @@
 <form action="top.php" method="post" >
-著者：<input type="text" name="inauthor" value="<?php echo $str1; ?>">
+タイトル：<input type="text" name="inauthor" value="<?php echo $str1; ?>">
 <br>
-タイトル：<input type="text" name="intitle" value="<?php echo $str2; ?>">
+著者：<input type="text" name="intitle" value="<?php echo $str2; ?>">
 <br>
 <input type="submit" value="検索">
 <br>
