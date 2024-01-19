83450a450309ed2e1ae5eba81b982bbb46732e3f
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-20 06:27:30 +0900

    a

--- src/default/header.php
+++ src/default/header.php
@@ -3,6 +3,6 @@
 <meta charset="UTF-8">
 <title>books</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="top.css">
+<link rel="stylesheet" href="css/top.css">
 </head>
 <body>
