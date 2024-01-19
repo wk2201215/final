55c92e9a55a18cf4e50e5a90cbd40ff6588222ad
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-19 21:22:49 +0900

    a

--- src/login-input.php
+++ src/login-input.php
@@ -1,11 +1,11 @@
 <?php session_start(); ?>
 <?php require 'default/header.php'; ?>
+<from action="login-output.php" method="post">
 <?php
     unset($_SESSION['customer']);
     if(isset($_GET['hogeA'])){
         echo '<p>',$_GET['hogeA'],'</p>';
     }
-    echo '<from action="login-output.php" method="post">';
     echo 'ユーザーID';
     echo '<input type="text" name="id">';
     echo '<br>';
@@ -29,8 +29,8 @@
     echo '<input type="submit" class="button" value="ログイン">';
     echo '<br>';
     echo 'アカウント新規作成は<a href="customer-input.php">こちら</a>';
-    echo '</from>';
 ?>
+</from>
 <script src="https://cdn.jsdelivr.net/npm/zxcvbn@4.4.2/dist/zxcvbn.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue-password-strength-meter@1.4.2/dist/vue-password-strength-meter.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.min.js"></script>
