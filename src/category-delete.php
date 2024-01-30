84ad03a8be7c83f690501f126ff2344231a64225
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-31 07:00:08 +0900

    a

--- src/category-delete.php
+++ src/category-delete.php
@@ -2,7 +2,7 @@
 <?php
 $pdo=new PDO($connect, USER, PASS);
 $sql=$pdo->prepare('delete from Category where category_id=?');
-$sql->execute([$_POST['id']]);
+$sql->execute([$_GET['id']]);
 header('Location:category.php');
 exit();
 ?>
--- src/category.php
+++ src/category.php
@@ -1,6 +1,5 @@
 <?php session_start(); ?>
 <?php require 'default/db-connect.php'; ?>
-<?php require 'default/api2.php'; ?>
 <?php require 'default/header-top.php'; ?>
 <?php require 'ba-ga-.php'; ?>
 <!-- <form action="registered-books.php" method="post"> -->
