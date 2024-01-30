d093863d905428a6af3201358ce87ac514ec9295
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-31 08:17:40 +0900

    a

--- src/category-delete.php
+++ src/category-delete.php
@@ -1,6 +1,8 @@
 <?php require 'default/db-connect.php'; ?>
 <?php
 $pdo=new PDO($connect, USER, PASS);
+$sql2=$pdo->prepare('update RegisteredBooks set category_id=? where  category_id=?');
+$sql2->execute([1,$_GET['id']]);
 $sql=$pdo->prepare('delete from Category where category_id=?');
 $sql->execute([$_GET['id']]);
 header('Location:category.php');
