f287c86141a601b0fb579954d037bdbd437f18b6
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-01-31 08:19:39 +0900

    a

--- src/r-b-d.php
+++ src/r-b-d.php
@@ -2,7 +2,7 @@
 <?php require 'default/db-connect.php'; ?>
 <?php
 $pdo=new PDO($connect, USER, PASS);
-$sql=$pdo->prepare('delete from RegisteredBooks where user_id=? and book_id');
+$sql=$pdo->prepare('delete from RegisteredBooks where user_id=? and book_id=?');
 $sql->execute([$_SESSION['user']['id'],$_GET['id']]);
 header('Location:registered-books.php');
 exit();
